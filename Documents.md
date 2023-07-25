4cf1c9edb922274de9ae47ecc6235aad729616db
biren067 <birendrabitian@gmail.com>
2023-07-25 15:39:06 +0530

    component added

---
+++ Documents.md
@@ -0,0 +1,3 @@
+npm install --global yarn
+yarn create next-app 
+yarn add next-auth
---
+++ tutorial_01/app/page.tsx
@@ -0,0 +1,12 @@
+import Nav from '@/components/Nav'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+        <Nav />
+        Bond
+      </div>
+    </main>
+  )
+}
