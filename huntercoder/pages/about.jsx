f4cc6e08066b74f61fc4740122c4572f21ab7db0
biren067 <birendrabitian@gmail.com>
2023-07-25 21:52:25 +0530

    module or page wise css

---
+++ huntercoder/pages/about.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from '../styles/about.module.css'
+function About() {
+    return (
+        <div className={styles.bg_cyan_color}>About page</div>
+    )
+}
+
+export default About
