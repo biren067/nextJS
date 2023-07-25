666d726fba8de3a29d5dc73285b477f9063c4e5a
biren067 <birendrabitian@gmail.com>
2023-07-25 22:02:46 +0530

    style jsx - component level

--- huntercoder/pages/about.jsx
+++ huntercoder/pages/about.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 import styles from '../styles/about.module.css'
+
 function About() {
+
     return (
-        <div className={styles.bg_cyan_color}>About page</div>
+        <>
+            <style jsx>
+                {`
+             .text_color{
+                 color:red;
+             }
+            `}
+            </style>
+            <div className={`${styles.bg_cyan_color} text_color`}> About page</div>
+            <div className="text_color"> About page</div>
+        </>
     )
 }
 
