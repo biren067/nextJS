05c867661cb91ef50ff5c1271a4bea2c49a43be9
biren067 <birendrabitian@gmail.com>
2023-07-25 19:12:57 +0530

    Remove old project

--- tutorial_02/app/about/page.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function about() {
-    return (
-        <div>This is About Page</div>
-    )
-}
-
-export default about
--- tutorial_02/components/Nav.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Nav() {
-    return (
-        <div>Navigation</div>
-    )
-}
-
-export default Nav
