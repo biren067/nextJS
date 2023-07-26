d054c7af33d83a42537a0653d0110a263bf1b4ca
biren067 <birendrabitian@gmail.com>
2023-07-26 22:02:17 +0530

    header and footer design

--- huntercoder/components/Footer.jsx
+++ huntercoder/components/Footer.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
-
+import Link from 'next/link'
+import styles from '../styles/Footer.module.css'
 function Footer() {
     return (
-        <div>Footer</div>
+        <>
+            <div className={styles.footer}>
+                <div className={`${styles.footer_primary} text-center`}>
+                    <Link href="/">Back To top</Link>
+                </div>
+                <div className={styles.footer_secondary}>
+                    <div className='py-10 mx-5'>
+                        All rights reserved © 2023
+                    </div>
+                </div>
+            </div>
+
+        </>
+
     )
 }
 
--- huntercoder/components/Header.jsx
+++ huntercoder/components/Header.jsx
@@ -1,15 +1,15 @@
 import React, { Fragment } from 'react'
 import Link from 'next/Link'
-// import '../styles/header.css'
+import styles from '../styles/Header.module.css'
 function Header() {
     return (
         <Fragment>
-            <nav className='mainnav'>
-                <ul className='list-group'>
-                    <Link href="/"><li className='list-items'>Home</li></Link>
-                    <Link href="/blog"><li className='list-items'>Blog</li></Link>
-                    <Link href="/contact"><li className='list-items'>Contact</li></Link>
-                    <Link href="/about"><li className='list-items'>About</li></Link>
+            <nav className={styles.mainnav}>
+                <ul className={styles.list_group}>
+                    <Link href="/"><li className={styles.list_item}>Home</li></Link>
+                    <Link href="/blog"><li className={styles.list_item}>Blog</li></Link>
+                    <Link href="/contact"><li className={styles.list_item}>Contact</li></Link>
+                    <Link href="/about"><li className={styles.list_item}>About</li></Link>
                 </ul>
             </nav>
         </Fragment>
