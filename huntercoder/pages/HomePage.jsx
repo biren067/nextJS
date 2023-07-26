ba708d9c5c6cf8b3654b408b454fbb2c6a416b73
biren067 <birendrabitian@gmail.com>
2023-07-27 01:14:34 +0530

    A bit more design

--- huntercoder/pages/HomePage.jsx
+++ huntercoder/pages/HomePage.jsx
@@ -1,26 +1,15 @@
 'useState'
 import React, { Fragment, useState, useEffect } from 'react'
-import Head from 'next/head'
-import Script from 'next/script'
-import Header from '../components/Header'
-import Footer from '../components/Footer'
-
+// import Head from 'next/head'
+// import Script from 'next/script'
+// import Header from '../components/Header'
+// import Footer from '../components/Footer'
+import styles from '../styles/Header.module.css'
 
 function HomePage() {
     const [blogList, getBlogList] = useState(null);
 
-    // const list_of_post = async () => {
 
-    //     try {
-    //         const res = await fetch('http://localhost:8000/api/blog/');
-    //         const BlogList = await res.json();
-    //         getBlogList(BlogList)
-    //         console.log(BlogList)
-
-    //     } catch (e) {
-    //         console.log(e);
-    //     }
-    // }
     useEffect(() => {
         const list_of_post = async () => {
 
@@ -38,9 +27,15 @@ function HomePage() {
     }, []);
     return (
         <Fragment>
-            {/* {blogList.map(item => (
-                <li key={item.title}>{item.content}</li>
-            ))} */}
+            {/* <ul className={styles.list_group}> */}
+            {blogList.map(item => (
+                <li key={item.title}>
+                    <div>{item.title}</div>
+                    <div>{item.content}</div>
+                    <div>{item.post_date}</div>
+                </li>
+            ))}
+            {/* </ul> */}
             {JSON.stringify(blogList)}
         </Fragment>
 
